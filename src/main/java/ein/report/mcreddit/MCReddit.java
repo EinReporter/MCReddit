16eb702fff955da1dcb21443410765ccb16aebc5
EinReporter <kontakt@ein.report>
2020-07-13 17:00:33 +0200

    Initial commit

---
+++ src/main/java/ein/report/mcreddit/MCReddit.java
@@ -0,0 +1,17 @@
+package ein.report.mcreddit;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MCReddit extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
